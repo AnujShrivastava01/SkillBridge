5b1543cba2ce7d90d4eda44ee3b026edcdfd6dc2
Anuj Shrivastava <anujshrivastava10e@gmail.com>
2025-09-14 19:29:27 +0530

    fx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,18 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Register service worker for PWA functionality using Vite PWA plugin
-import { registerSW } from 'virtual:pwa-register';
-
-if ('serviceWorker' in navigator) {
-  registerSW({
-    onNeedRefresh() {
-      console.log('New content available, please refresh');
-    },
-    onOfflineReady() {
-      console.log('App ready to work offline');
-    },
-  });
-}
-
 createRoot(document.getElementById("root")!).render(<App />);
